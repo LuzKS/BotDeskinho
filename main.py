24c3e7b86e1546003838d333a18164b624e4efbd
Lucas Santos <lucas.esscontato@gmail.com>
2025-08-25 15:23:21 -0300

    corrigindo respostas infinitas

--- main.py
+++ main.py
@@ -5,44 +5,46 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Inicializa o bot com as permissões (intents) necessárias
 intents = discord.Intents.default()
-intents.message_content = True  # Permite que o bot leia o conteúdo das mensagens
+intents.message_content = True  # deixa o bot ler o conteúdo das mensagens
 client = discord.Client(intents=intents)
 
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-# O evento que dispara quando o bot está online
+
 @client.event
 async def on_ready():
     print(f'Bot logado como {client.user}')
 
-# O evento que dispara quando uma nova mensagem é enviada
+
+
 @client.event
 async def on_message(message):
     print(message.author)
-    # Ignora mensagens do próprio bot para evitar um loop infinito
+    print(message.channel)
+    # ignora mensagens do próprio bot para evitar um loop infinito
     if message.author == client.user:
         return
 
-    # Condição para disparar o webhook.
     if message.content.lower():
-        try:
-            # Envia uma requisição POST para o webhook do n8n
-            response = requests.post(
-                WEBHOOK_URL,
-                json={"user": message.author.display_name,
-                        "user_id": str(message.author.id),
-                        "message": message.content,
-                        "message_id": str(message.id),
-                        "channel_id": str(message.channel.id),
-                        "server_id": str(message.guild.id) if message.guild else None
-                      }
-            )
-            response.raise_for_status() # Lança um erro se a requisição falhar
-            await message.channel.send("Ticket acionado!")
-        except requests.exceptions.RequestException as e:
-            await message.channel.send(f"❌ Erro ao acionar o webhook: {e}")
+        if str(message.channel) == "comandos":
+            try:
+                # envia uma requisição POST para o webhook do n8n
+                response = requests.post(
+                    WEBHOOK_URL,
+                    json={  "username": message.author.name,
+                            "user": message.author.display_name,
+                            "user_id": str(message.author.id),
+                            "message": message.content,
+                            "message_id": str(message.id),
+                            "channel_id": str(message.channel.id),
+                            "server_id": str(message.guild.id) if message.guild else None
+                        }
+                )
+                response.raise_for_status() 
+                await message.channel.send("Ticket acionado!")
+            except requests.exceptions.RequestException as e:
+                await message.channel.send(f"❌ Erro ao acionar o webhook: {e}")
 
 client.run(DISCORD_TOKEN)
